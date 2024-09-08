fa6e431ec77367f001b9ad28d446f650bf920fa9
BairM1247 <mba84@mail.ru>
2024-09-09 01:43:47 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+my_dict = {'Brandon': 1983, 'Michel': 2003, 'Albert': 1967 }
+print (my_dict)
+print (my_dict ['Michel'])
+print (my_dict.get('Biatreasse'))
+my_dict.update ({'Clarissa': 1999, 'Milena': 2021})
+print (my_dict)
+a = my_dict.pop ('Michel')
+print (a)
+print(my_dict)
+
+my_set_ = {1, 3, 4, 3, 1, 7, 5, "Max", 'lex'}
+print (my_set_)
+my_set_.add (9)
+my_set_.add ('Mila')
+print (my_set_)
+my_set_.remove (3)
+print (my_set_)
